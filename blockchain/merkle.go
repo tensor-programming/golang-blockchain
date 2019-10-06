b673e7de25a6d8cea0c1becfd41288895d811a35
Tensor-Programming <abeltensor@tensor-programming.com>
2019-10-05 20:14:58 -0400

    Update merkle.go

--- blockchain/merkle.go
+++ blockchain/merkle.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package blockchain
 
 import (
@@ -64,70 +63,3 @@ func NewMerkleTree(data [][]byte) *MerkleTree {
 
 	return &tree
 }
-=======
-package blockchain
-
-import (
-	"crypto/sha256"
-	"log"
-)
-
-type MerkleTree struct {
-	RootNode *MerkleNode
-}
-
-type MerkleNode struct {
-	Left  *MerkleNode
-	Right *MerkleNode
-	Data  []byte
-}
-
-func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
-	node := MerkleNode{}
-
-	if left == nil && right == nil {
-		hash := sha256.Sum256(data)
-		node.Data = hash[:]
-	} else {
-		prevHashes := append(left.Data, right.Data...)
-		hash := sha256.Sum256(prevHashes)
-		node.Data = hash[:]
-	}
-
-	node.Left = left
-	node.Right = right
-
-	return &node
-}
-
-func NewMerkleTree(data [][]byte) *MerkleTree {
-	var nodes []MerkleNode
-
-	for _, dat := range data {
-		node := NewMerkleNode(nil, nil, dat)
-		nodes = append(nodes, *node)
-	}
-
-	if len(nodes) == 0 {
-		log.Panic("No merkel nodes")
-	}
-
-	for len(nodes) > 1 {
-		if len(nodes)%2 != 0 {
-			nodes = append(nodes, nodes[len(nodes)-1])
-		}
-
-		var level []MerkleNode
-		for i := 0; i < len(nodes); i += 2 {
-			node := NewMerkleNode(&nodes[i], &nodes[i+1], nil)
-			level = append(level, *node)
-		}
-
-		nodes = level
-	}
-
-	tree := MerkleTree{&nodes[0]}
-
-	return &tree
-}
->>>>>>> e14978d1b38f52129396238a7d4d8709d0dbb115
